7f368583b6bf0acd3266770adcaa9803e3ef47c3
Z3Programming <z3programming@gmail.com>
2018-12-27 18:02:26 -0500

    set random color array and properties

--- SayHello/SayHello/MainPage.xaml.cs
+++ SayHello/SayHello/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,13 +23,35 @@ namespace SayHello
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        Random rand;
+
         public MainPage()
         {
             this.InitializeComponent();
+            rand = new Random();
+        }
+
+        private byte[] GetRandomBytes(int n)
+        {
+            //fill an array of bytes length "n" with random numbers
+            var randomBytes = new byte[n];
+            rand.NextBytes(randomBytes);
+
+            return randomBytes;
         }
 
         private void BtnSayHello_Click(object sender, RoutedEventArgs e)
         {
+
+            byte[] rgb = GetRandomBytes(3);
+
+            //create a solid bolor brush using the three random numbers
+            var randomColorBrush = new SolidColorBrush(Color.FromArgb(255,rgb[0],rgb[1],rgb[2]));
+
+            //set both the text color and the text box border to the random color
+            txtText.BorderBrush = randomColorBrush;
+            txtText.Foreground = randomColorBrush;
 
         }
     }
